4ee4fc84053c793ab28fb5730088925b3414cac4
RoGCalderon <guadac0516@gmail.com>
2023-09-25 22:56:59 -0300

    Cambios estilos

--- utils/connection.js
+++ utils/connection.js
@@ -3,7 +3,7 @@ import mysql2 from "mysql2";
 const connection = mysql2.createConnection({
   host: "localhost",
   user: "root",
-  password: "123456789KIKI",
+  password: "root",//"123456789KIKI",
   database: "proyecto_node",
 });
 
